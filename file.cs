cb594e91e3404151fac6aa134f7da4773762b7e8
SergeySavchenka <118177405+SergeySavchenka@users.noreply.github.com>
2023-03-28 15:07:42 +0300

    Create file.cs

---
+++ file.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab02
+{
+internal class Program
+{
+static void Main(string[] args)
+{
+    int c = Convert.ToInt32(Console.ReadLine());
+    int m = Convert.ToInt32(Console.ReadLine());
+    int e = Convert.ToInt32(Console.ReadLine());
+    int A = Convert.ToInt32(Console.ReadLine());
+}
+}
+}
